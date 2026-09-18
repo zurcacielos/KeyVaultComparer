6c8da9650d00982c16975f1a6008c315685e7c44
zurcacielos <fabianse@gmail.com>
2026-09-18 00:38:46 -0400

    style: replace Help icon with explicit text button

--- keyvaultcomparer-ui/src/App.vue
+++ keyvaultcomparer-ui/src/App.vue
@@ -1319,12 +1319,10 @@ const getCellClasses = (statusObj: SecretValueStatus | undefined) => {
       <div class="flex items-center gap-3">
         <button 
           @click="showHelpDialog = true" 
-          class="p-1.5 text-slate-400 hover:bg-slate-100 hover:text-slate-600 rounded-md transition-colors"
+          class="px-3 py-1.5 text-sm font-semibold text-slate-500 hover:bg-slate-100 hover:text-slate-700 rounded-md transition-colors"
           title="Help & About"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
-          </svg>
+          Help
         </button>
 
         <a 
